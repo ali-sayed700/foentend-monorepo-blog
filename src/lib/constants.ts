bd023186c14fff65abb259a6964bf6048b1d92d5
ali-sayed700 <aly.sayed9595@gmail.com>
2025-09-15 12:50:25 +0300

    edit url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
-export const BACKEND_URL = "http://localhost:8000"
+// export const BACKEND_URL = "http://localhost:8000"
+export const BACKEND_URL = 'https://backend-monorepo-blog.onrender.com';
 
 export const DEFAULT_PAGE_SIZE = 12;
